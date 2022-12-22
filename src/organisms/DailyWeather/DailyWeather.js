3111b6a281cb58db2186d750ab7baa4db76ff915
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-12-22 18:47:29 +0530

    Displays Max, Min Temp icon

--- src/organisms/DailyWeather/DailyWeather.js
+++ src/organisms/DailyWeather/DailyWeather.js
@@ -8,26 +8,40 @@ const DailyWeather = ({ latitude, longitude }) => {
   useEffect(() => {
     axios
       .get(
-        `https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${latitude}&lon=${longitude}&appid=f57e4860fc24f86437748df4654445a9&cnt=1&units=metric`
+        `https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${latitude}&lon=${longitude}&appid=f57e4860fc24f86437748df4654445a9&cnt=7&units=metric`
       )
       .then((response) => {
         setApiData(response.data.list);
       });
   }, [latitude, longitude]);
-  console.log(apiData);
+  // console.log(apiData);
+  const date = new Date();
+  console.log(date);
   return (
     <div className="dailyWeather_Container">
       {apiData.map((eachDayObj) => {
+        console.log(eachDayObj.dt);
         return (
           <div className="dailyWeather_Container_SingleBlock">
-            <h2>Friday</h2>
+            <h2>{new Date(eachDayObj.dt).getDay()}</h2>
             <div className="dailyWeather_Container_SingleBlock_MaxMinContainer">
               <p>
                 <b>{Math.round(eachDayObj.temp.max)}°C</b>
               </p>
               <p>{Math.round(eachDayObj.temp.min)}°C</p>
             </div>
-            <img src="https://picsum.photos/id/237/60/60" />
+
+            {eachDayObj.weather.map((eachObj) => {
+              return (
+                <>
+                  <img
+                    alt="img"
+                    src={`http://openweathermap.org/img/w/${eachObj.icon}.png`}
+                  />
+                  <p>{eachObj.main}</p>
+                </>
+              );
+            })}
           </div>
         );
       })}
