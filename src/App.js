226a2ed1c9605c423948d93f49479504b255ed8a
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-12-22 16:12:50 +0530

    Adds responsive template of Dailyforecast container and its child

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import SearchBar from "./organisms/SearchBar/SearchBar";
+import axios from "axios";
+import DailyWeather from "./organisms/DailyWeather/DailyWeather";
 
 function App() {
   const [latitude, setLatitude] = useState();
@@ -11,12 +13,21 @@ function App() {
     setLatitude(position.coords.latitude);
   });
 
-  latitude && longitude && console.log(latitude, longitude);
+  axios
+    .get(
+      `https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${latitude}&lon=${longitude}&appid=f57e4860fc24f86437748df4654445a9&cnt=7&units=metric`
+    )
+    .then((response) => {
+      console.log(response.data.list);
+    });
 
+  // const date = new Date(1671690600);
+  // console.log(date);
   return (
     <div className="App">
       <div className="container">
         <SearchBar />
+        <DailyWeather />
       </div>
     </div>
   );
---
+++ src/organisms/DailyWeather/DailyWeather.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./DailyWeather.css";
+import { Text } from "@mantine/core";
+
+const DailyWeather = () => {
+  return (
+    <div className="dailyWeather_Container">
+      <div className="dailyWeather_Container_SingleBlock"></div>
+    </div>
+  );
+};
+
+export default DailyWeather;
